19113917ed79226d248189b0b82c4470b669f929
mazuralexey93 <mazuralexey93@gmail.com>
2022-01-05 21:58:58 +0300

    starting gunicorn

--- djangoproject/djangoproject/settings/prod.py
+++ djangoproject/djangoproject/settings/prod.py
@@ -3,5 +3,5 @@ from .base import *
 
 ALLOWED_HOSTS = ['*']
 DEBUG = False
-# SECRET_KEY = 'django-insecure-+3*i*p!386!%^xd-h7jn!r8bq5n*e7xu+#3(x*&)1c#x_hs_vn'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-+3*i*p!386!%^xd-h7jn!r8bq5n*e7xu+#3(x*&)1c#x_hs_vn'
+# SECRET_KEY = os.environ['SECRET_KEY']
--- djangoproject/djangoproject/settings/prod_int.py
+++ djangoproject/djangoproject/settings/prod_int.py
@@ -6,9 +6,10 @@ DATABASES = {
             'NAME': 'test',
             'USER': 'user',
             'PASSWORD': 'password',
-            'HOST': '127.0.0.1',
-            'PORT': '54322',
+            'HOST': 'db',
+            'PORT': '5432',
     }
 }
+
 DEBUG = True
 SITE_ID = 1
